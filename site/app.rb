e52e30fb8ca07c8119d5b886aa7dcd512dcc1cb7
itggot-alexander-kjellberg <alexander.kjellberg2@elev.ga.ntig.se>
2020-03-05 10:42:17 +0100

    Gjorde så man kan göra blogposts på main sidan, det som skrivs ut är själva innehållet i inlägget samt vem som skapat det. Jag har gjort en sida där man ska kunna logga in. länkat databasen. gjort css fil och gjort lite layout med den.

--- site/app.rb
+++ site/app.rb
@@ -1,15 +1,33 @@
 require 'sinatra'
 require 'slim'
+require 'sqlite3'
 
 before do
     session[:user] = 1
+    result = []
 end
 
 get('/') do
-    slim(:setup)
+    db = SQLite3::Database.new("db/db.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM blogpost")
+    slim(:setup,locals:{blogposts:result})
     
 end
 
 get('/content') do
     slim(:"content/index")
+end
+
+post('/blogpost/new') do
+    user_id = session[:user]
+    content = params["content"]
+    db = SQLite3::Database.new("db/db.db")
+    db.execute("INSERT INTO blogpost (content, user_id) VALUES (?,?)",content, user_id)
+
+    redirect('/')
+end
+
+get('/user/index') do
+   slim(:"user/index") 
 end
