6fa82837960f63e378aee187d7f2ca3fbc0598e5
itggot-alexander-kjellberg <alexander.kjellberg2@elev.ga.ntig.se>
2020-03-26 10:59:45 +0100

    Påbörjade skapandet av en form som ska kunna ta bort content divs. Tog bort lite html som inte behövdes då den koden redan fanns vid min yield. Skaffade mig en "after". Gjorde så att man kan skapa ett konto som läggs till i databasen. Gjorde så att man kan logga in på sidan med det skapade kontot. Satte ut kommentarer som beskriver vad som inte fungerar.
    Kan inte logga in på min  webb-github så får skriva dokumentation här.
    
    Vad gick bra?
    Det gick bra tillslut att få så man kan skapa konton samt logga in på konton.
    
    Vad gick dåligt?
    När jag väl fick till så att man kunde logga in så slutade det fungera att lägga till content. Jag tror att det har och göra med user_id
    
    Vad ska göras nästa gång?
    Fixa så att man kan göra "inlägg" som olika users. Fixa så att man kan logga ut. Fixa så att man kan ta bort inlägg utan att gå in i databasen.

--- site/app.rb
+++ site/app.rb
@@ -3,8 +3,12 @@ require 'slim'
 require 'sqlite3'
 
 before do
-    session[:user] = 1
+    # session[:user] = 1
     result = []
+end
+
+after do
+    p session[:user]
 end
 
 get('/') do
@@ -19,15 +23,63 @@ get('/content') do
     slim(:"content/index")
 end
 
+
+# Fungerar inte.
+# Efter att login lades till så slutade det fungera att lägga till content
 post('/blogpost/new') do
     user_id = session[:user]
     content = params["content"]
     db = SQLite3::Database.new("db/db.db")
-    db.execute("INSERT INTO blogpost (content, user_id) VALUES (?,?)",content, user_id)
-
+    db.execute("INSERT INTO blogpost (content, id) VALUES (?,?)",content,id)
+    
     redirect('/')
 end
 
 get('/user/index') do
-   slim(:"user/index") 
+    slim(:"user/index") 
+end
+
+
+# Skapa konto, FUNGERAR DELVIS.
+# Ingenting kollar om newPass == confPass
+post('/user/skapaKonto') do
+    newUsername = params['newUsername'] 
+    newPassword = params['newPassword']
+    confirmPassword = params['confirmPassword']
+    
+    
+    db = SQLite3::Database.new("db/db.db")
+    db.execute("INSERT INTO user (username, password) VALUES (?,?)",newUsername, newPassword)
+    
+    redirect('/')
+end
+
+# Fungerar inte delvis.
+# Kollar ej om det är korrekt user med korrekt pass
+post('/user/login') do
+    loginUsername = params['username'] 
+    loginPassword = params['password'] 
+    
+    
+    db = SQLite3::Database.new("db/db.db")
+    result = db.execute("SELECT password,id FROM user WHERE username = ?",loginUsername)
+    
+    p result
+    p loginPassword
+    if result[0][0] == loginPassword
+        session[:user] = result[0][1]
+    else
+        p "fel"
+    end
+    redirect('/')
+end
+
+# Fungerar inte.
+# Händer inget vid klick
+post('/removeContent') do
+
+    db = SQLite3::Database.new("db/db.db")
+    db.execute("DELETE FROM blogpost WHERE content",item)
+
+    redirect('/')
 end
